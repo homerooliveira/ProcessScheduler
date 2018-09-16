cdce38aa2f69c12226785b2aae5a5a7fa906a3a9
homerooliveira <homero.santos.001@acad.pucrs.br>
2018-09-16 20:34:16 -0300

    alot of changes

---
+++ ProcessScheduler/Array+sum+average.swift
@@ -0,0 +1,9 @@
+//
+//  Array+Calculation.swift
+//  ProcessScheduler
+//
+//  Created by Homero Oliveira on 16/09/18.
+//  Copyright © 2018 Homero Oliveira. All rights reserved.
+//
+
+import Foundation
