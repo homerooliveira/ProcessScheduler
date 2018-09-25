3d663efb88bbbca29215fa9e66d0b81f45eba5da
homerooliveira <homero.santos.001@acad.pucrs.br>
2018-09-25 01:02:40 -0300

    added files to playground

--- ProcessScheduler.playground/Contents.swift
+++ ProcessScheduler.playground/Contents.swift
@@ -3,18 +3,25 @@
 import Foundation
 import ProcessSchedulerCore
 
-var proccess = [
-    Process(id: 1, arrivalTime: 3, executionTime: 10, priority: 2),
-    Process(id: 2, arrivalTime: 5, executionTime: 12, priority: 1),
-    Process(id: 3, arrivalTime: 9, executionTime: 15, priority: 2),
-    Process(id: 4, arrivalTime: 11, executionTime: 15, priority: 1),
-    Process(id: 5, arrivalTime: 12, executionTime: 8, priority: 5,
-            accessToInOutOperations: [2])
-]
+//var proccess = [
+//    Process(id: 1, arrivalTime: 3, executionTime: 10, priority: 2),
+//    Process(id: 2, arrivalTime: 5, executionTime: 12, priority: 1),
+//    Process(id: 3, arrivalTime: 9, executionTime: 15, priority: 2),
+//    Process(id: 4, arrivalTime: 11, executionTime: 15, priority: 1),
+//    Process(id: 5, arrivalTime: 12, executionTime: 8, priority: 5,
+//            accessToInOutOperations: [2])
+//]
+//let input = ExecutionInput(processes: proccess, quantum: 3)
+
+
+guard let fileUrl = Bundle.main.url(forResource: "trab-so1-teste1SR", withExtension: "txt") else { fatalError() }
+
+let input = FileReader(fileUrl: fileUrl).executionInput
 
 let processScheduller = ProcessScheduler()
-let input = ExecutionInput(processes: proccess, quantum: 3)
+
 let output = processScheduller.execute(input: input)
+
 print(output.output)
 print("average wait Time = \(output.averageWaitTime)")
 print("average response Time = \(output.averageResponseTime)")
