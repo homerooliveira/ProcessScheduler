d80018becfe529b6f256e34d31952427056c72cf
homerooliveira <homero.santos.001@acad.pucrs.br>
2018-09-25 00:44:35 -0300

    added playground to project and removed main.swift

--- ProcessScheduler.playground/Contents.swift
+++ ProcessScheduler.playground/Contents.swift
@@ -1,5 +1,20 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import Foundation
+import ProcessSchedulerCore
 
-var str = "Hello, playground"
+var proccess = [
+    Process(id: 1, arrivalTime: 3, executionTime: 10, priority: 2),
+    Process(id: 2, arrivalTime: 5, executionTime: 12, priority: 1),
+    Process(id: 3, arrivalTime: 9, executionTime: 15, priority: 2),
+    Process(id: 4, arrivalTime: 11, executionTime: 15, priority: 1),
+    Process(id: 5, arrivalTime: 12, executionTime: 8, priority: 5,
+            accessToInOutOperations: [2])
+]
+
+let processScheduller = ProcessScheduler()
+let input = ExecutionInput(processes: proccess, quantum: 3)
+let output = processScheduller.execute(input: input)
+print(output.output)
+print("average wait Time = \(output.averageWaitTime)")
+print("average response Time = \(output.averageResponseTime)")
---
+++ ProcessSchedulerCore/FileReader.swift
@@ -0,0 +1,9 @@
+//
+//  FileReader.swift
+//  ProcessSchedulerCore
+//
+//  Created by Homero Oliveira on 25/09/18.
+//  Copyright © 2018 Homero Oliveira. All rights reserved.
+//
+
+import Foundation
