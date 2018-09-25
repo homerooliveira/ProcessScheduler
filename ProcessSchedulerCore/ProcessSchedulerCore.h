60ddb63097aad884a2ca10905f1c6675bb404731
homerooliveira <homero.santos.001@acad.pucrs.br>
2018-09-25 00:41:39 -0300

    create project as framework

---
+++ ProcessSchedulerCore/ProcessSchedulerCore.h
@@ -0,0 +1,19 @@
+//
+//  ProcessSchedulerCore.h
+//  ProcessSchedulerCore
+//
+//  Created by Homero Oliveira on 25/09/18.
+//  Copyright © 2018 Homero Oliveira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProcessSchedulerCore.
+FOUNDATION_EXPORT double ProcessSchedulerCoreVersionNumber;
+
+//! Project version string for ProcessSchedulerCore.
+FOUNDATION_EXPORT const unsigned char ProcessSchedulerCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProcessSchedulerCore/PublicHeader.h>
+
+
