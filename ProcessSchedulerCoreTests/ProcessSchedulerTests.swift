195954ebbc7812b1dda1bd912793ca610b16cac1
Homero Oliveira <homero.santos.001@acad.pucrs.br>
2018-09-29 16:44:29 -0300

    added new test

--- ProcessSchedulerCoreTests/ProcessSchedulerTests.swift
+++ ProcessSchedulerCoreTests/ProcessSchedulerTests.swift
@@ -10,25 +10,40 @@ import XCTest
 @testable import ProcessSchedulerCore
 
 class ProcessesSchedulerTests: XCTestCase {
-
+    
     var processScheduler: ProcessScheduler!
-
+    
     override func setUp() {
         super.setUp()
         processScheduler = ProcessScheduler()
     }
-
+    
     func testTwoProcessWithDiferentsPriority() {
         let processes = [
             Process(id: 1, arrivalTime: 3, executionTime: 10, priority: 2),
             Process(id: 2, arrivalTime: 5, executionTime: 12, priority: 1)
         ]
-
+        
         let input = ExecutionInput(processes: processes, quantum: 3)
-
+        
         let output = processScheduler.execute(input: input)
-
+        
         XCTAssertEqual("---C1C222C222C222C222C11C111C111C1", output.output)
+    }
+    
+    func testTwoProcessWithSamePriorityAndSameArrivalTime() {
+        let processes = [
+                Process(id: 1, arrivalTime: 1, executionTime: 3, priority: 2),
+                Process(id: 2, arrivalTime: 1, executionTime: 3, priority: 1),
+                Process(id: 3, arrivalTime: 1, executionTime: 3, priority: 1)
+        ]
+        
+        let input = ExecutionInput(processes: processes,
+                                   quantum: 3)
+        
+        let output = processScheduler.execute(input: input)
+        
+        XCTAssertEqual("-C222C333C111", output.output)
     }
     
     func testFromMoodle() {
@@ -47,5 +62,5 @@ class ProcessesSchedulerTests: XCTestCase {
         
         XCTAssertEqual("---C1C222C222C444C222C444C222C444C444C444C11C333C111C333C111C333C1C333C333C55C---C5C555C55", output.output)
     }
-
+    
 }
